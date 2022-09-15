f8586fa6da3bf7c75a6557a05aadf91d2a7dfd8c
RomfallOfStarhv <76827164+RomfallOfStarhv@users.noreply.github.com>
2022-09-15 08:43:21 +0200

    Contact + Slider

---
+++ Slider/assets/JS/carrousel.js
@@ -0,0 +1,13 @@
+var slide = new Array("assets/img/1.png", "assets/img/2.png", "assets/img/3.png");
+var numero = 0;
+
+function ChangeSlide(sens) {
+    
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
+
